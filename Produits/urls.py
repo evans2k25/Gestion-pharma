4e3462cbaa446bf94164bbfcf76dbc6e06c97133
jean-francois-goly <jean-francois-goly@iipea.com>
2025-05-19 11:56:00 +0000

    updat 19/05

--- Produits/urls.py
+++ Produits/urls.py
@@ -1,25 +1,16 @@
-from turtle import done
 from django.urls import path
-from .views import *
-from Produits import views
+from .views import Affichage, AjoutProduit, Update_donnees 
+
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import path
-
-
-
-
-
 
 urlpatterns = [
-   # path('',home,name='home')
-   
-   path('',Affichage.as_view(),name='home'),
-   path('Ajout/',AjoutProduit.as_view(),name='Ajout'),
-   path('modification/<int:pk>/',Update_donnees.as_view(),name='modification'),
- 
-  
+    path('', Affichage.as_view(), name='home'),
+    path('Ajout/', AjoutProduit.as_view(), name='Ajout'),
+    path('modification/<int:pk>/', Update_donnees.as_view(), name='modification'),
+   #  path('Detail/<int:pk>/', DetailProduit.as_view(), name='DetailProduit'),
+    
+]
 
-   
-] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Produits/views.py
+++ Produits/views.py
@@ -1,64 +1,32 @@
-from django.shortcuts import render,redirect, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse_lazy
-from .models import Produit, Category
-from django.contrib import messages
-from datetime import datetime
-from django.core.files.storage import FileSystemStorage
-from django.views.generic import ListView, CreateView ,UpdateView
-from .forms import AjoutProduit
-from django.http import JsonResponse
-from django.views.decorators.http import require_http_methods
 from .models import Produit
-from .models import Category
-from .forms import AjoutProduit
-
-
-
-# Create your views here.
-#def home(request):
-    
-    # recuperation des données
- #   donnees = Produit.objects.all()
-    
- #   context = {
-  #      'donnees':donnees
-   # }
-    
-    #return render(request, 'home.html',context)
-
-
+from django.views.generic import ListView, CreateView, UpdateView,DetailView
+from .forms import AjoutProduit  # Nom corrigé
 
 class Affichage(ListView):
-    
-    
     model = Produit
-    
-    #affichage du template
     template_name = 'home.html'
-    
-    #recuperation des données
     queryset = Produit.objects.all()
-    
-#class d'ajout des données avec formulaire généré
+
 class AjoutProduit(CreateView):
-    
-    #utlisitation du model 
     model = Produit
-    #specification du formulaire
     form_class = AjoutProduit
-    #affichage du template
-    template_name ='Ajout-donnee.html'
+    template_name = 'Ajout-donnee.html'
     success_url = reverse_lazy('home')
-    
-#classe  pour modifier les données
+
 class Update_donnees(UpdateView):
-    #recuperation du model
     model = Produit
-    #specipication du formulaire
     form_class = AjoutProduit
-    #affichage du template
     template_name = 'modification.html'
     success_url = reverse_lazy('home')
-    
-    
 
+    def form_valid(self, form):
+        # Optionnel : Logique supplémentaire avant la sauvegarde
+        return super().form_valid(form)
+    
+# class DetailProduit(DetailView):
+#     model = Produit
+#     template_name = 'detailproduit.html'  # à créer
+#     context_object_name = 'produit'
+#     success_url = reverse_lazy('home')
